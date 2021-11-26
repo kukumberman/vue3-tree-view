a53ffb64081f673435b393f1122e1f07bc7d6c73
Dima <strongbaby1337@gmail.com>
2021-11-26 23:49:10 +0200

    added basic .json file selection

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,21 @@
 <template>
-  <div class="container">
-    <TreeBrowser
-      :node="hierarchy"
-      @objectClicked="handleClick"
-      style="overflow-y: scroll; height: 100vh; width: 50vw;"
-    />
-    <Inspector :node="selectedNode" />
+  <div>
+    <input type="file" accept=".json" @change="onFileChange">
+    <div class="container">
+      <TreeBrowser
+        v-if="hierarchy !== null"
+        :node="hierarchy"
+        @objectClicked="handleClick"
+        style="overflow-y: scroll; height: 100vh; width: 50vw;"
+      />
+      <Inspector :node="selectedNode" />
+    </div>
   </div>
 </template>
 
 <script>
 import TreeBrowser from "./components/TreeBrowser.vue"
 import Inspector from "./components/Inspector.vue"
-
-import data from "./json/OfflinePlayer(Clone).json"
-//import data from "./data.json"
 
 export default {
   name: 'App',
@@ -23,25 +24,31 @@ export default {
   },
   data() {
     return {
-      hierarchy: data,
+      hierarchy: null,
       selectedNode: null
     }
   },
   methods: {
     handleClick(stack) {
       // todo: find better way
-      let result = data
+      let result = this.hierarchy
       for (let i = 1; i < stack.length; i++) {
         result = result.Childrens.find(c => c.Name == stack[i])
       }
       this.selectedNode = result
-    }
+    },
+    onFileChange(event) {
+      this.readFile(event.target.files[0]).then(text => this.hierarchy = JSON.parse(text))
+    },
+    readFile(file) {
+      return new Promise((resolve, reject) => {
+        const reader = new FileReader()
+        reader.onload = () => resolve(reader.result)
+        reader.onerror = () => reject(reader.error)
+        reader.readAsText(file)
+      })
+    },
   },
-  computed: {
-    clickedElement() {
-      return this.stack.join(" > ")
-    }
-  }
 }
 </script>
 
